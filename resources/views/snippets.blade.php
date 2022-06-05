1745f27e7998174f48e6c0e5c50ba842dc425482
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-05 13:55:34 +0700

    create temp page snippets

---
+++ resources/views/snippets.blade.php
@@ -0,0 +1,10 @@
+{{-- LOAD DATA --}}
+{{-- @dd($posts) --}}
+
+@extends('layouts.main')
+
+@section('container')
+    <h1 class="text-3xl font-bold underline text-white">
+        Hello snippets!
+    </h1>
+@endsection
