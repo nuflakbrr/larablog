0b7fc03d46d0e8a7b8d42bb26857f3f23de9de6e
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-05 13:56:29 +0700

    fix

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -51,10 +51,10 @@
                                             <div>
                                                 {{-- Title --}}
                                                 <div class="mb-2">
-                                                    <a href="/blog/{{ $post['slug'] }}">
+                                                    <a href="/blog/{{ $post->slug }}">
                                                         <h3
                                                             class="text-xl font-bold leading-7 tracking-tight text-white hover:text-sky-500">
-                                                            {{ $post['title'] }}
+                                                            {{ $post->title }}
                                                         </h3>
                                                     </a>
                                                 </div>
@@ -70,15 +70,15 @@
                                                                     d="M256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512zM232 256C232 264 236 271.5 242.7 275.1L338.7 339.1C349.7 347.3 364.6 344.3 371.1 333.3C379.3 322.3 376.3 307.4 365.3 300L280 243.2V120C280 106.7 269.3 96 255.1 96C242.7 96 231.1 106.7 231.1 120L232 256z" />
                                                             </svg>
                                                         </span>
-                                                        <time dateTime={{ $post['date'] }} class="text-zinc-50">
-                                                            {{ $post['date'] }}
+                                                        <time dateTime={{ $post->published_at }} class="text-zinc-50">
+                                                            {{ $post->date }}
                                                         </time>
                                                     </div>
                                                     {{-- Date & Tag End --}}
 
                                                     {{-- Summary --}}
                                                     <div class="prose max-w-none leading-5 text-gray-400">
-                                                        {{ $post['body'] }}
+                                                        {{ $post->excerpt }}
                                                     </div>
                                                     {{-- Summary End --}}
                                                 </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,8 +6,8 @@
 @section('container')
     <article>
         <div class="flex flex-col items-center justify-center mx-auto pb-8 border-b-2 border-gray-600">
-            <h4 class="text-lg text-gray-400">{{ $post['date'] }}</h4>
-            <h1 class="text-5xl text-white font-bold">{{ $post['title'] }}</h1>
+            <h4 class="text-lg text-gray-400">{{ $post->publised_at }}</h4>
+            <h1 class="text-5xl text-white font-bold">{{ $post->title }}</h1>
         </div>
         <div class="grid grid-cols-8 gap-6 md:py-10 lg:py-10 xl:py-0">
             <div class="col-span-8 md:col-span-8 lg:col-span-8 xl:col-span-2 my-2">
@@ -18,7 +18,7 @@
                             Penulis
                         </h4>
                         <h2 class="mb-1 max-w-md text-xl font-bold capitalize text-white lg:text-xl">
-                            {{ $post['author'] }}
+                            {{ $post->author }}
                         </h2>
                         <p class="max-w-6xl text-base font-medium text-slate-500 lg:text-sm">
                             Saya bersemangat memberikan kontribusi untuk memberikan pengetahuan teknologi bagi semua
@@ -35,15 +35,24 @@
                             Kategori
                         </h4>
                         <h2 class="mb-1 max-w-md text-xl font-bold capitalize text-white lg:text-xl">
-                            {{ $post['category'] }}
+                            {{ $post->category }}
                         </h2>
                     </div>
                 </div> --}}
                 {{-- Category End --}}
 
+            </div>
+
+            <div class="col-span-8 md:col-span-8 lg:col-span-8 xl:col-span-6">
+                <div class="my-2 text-white">
+                    {!! $post->body !!}
+                </div>
+            </div>
+
+            <div class="col-span-8 md:col-span-8 lg:col-span-8 xl:col-span-2 my-2">
                 {{-- Back to blog --}}
                 <div>
-                    <div class="w-full px-4 py-5">
+                    <div class="w-full px-4 py-5 xl:py-0 border-t-2 border-gray-600 xl:border-t-0">
                         <a href="/blog"
                             class="max-w-6xl text-base font-medium text-slate-500 hover:text-sky-500 lg:text-md">
                             &larr; Kembali ke Catatan
@@ -51,12 +60,6 @@
                     </div>
                 </div>
                 {{-- Back to blog End --}}
-            </div>
-
-            <div class="col-span-8 md:col-span-8 lg:col-span-8 xl:col-span-6">
-                <div class="my-2">
-                    <p class="text-xl text-white">{{ $post['body'] }}</p>
-                </div>
             </div>
         </div>
     </article>
