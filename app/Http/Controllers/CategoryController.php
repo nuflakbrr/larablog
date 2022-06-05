eb5efa4b7a78178f436e2648ad0420a2c772dc3d
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-05 21:02:55 +0700

    create category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('tags', [
+            'path' => '/tags',
+            'title' => 'Kategori -',
+            'posts' => Category::all()
+        ]);
+    }
+
+    public function singleCategory(Category $category)
+    {
+        return view('tag', [
+            'path' => '/tags',
+            'title' => $category->name . ' -',
+            'posts' => $category->post,
+            'category' => $category->name
+        ]);
+    }
+}
