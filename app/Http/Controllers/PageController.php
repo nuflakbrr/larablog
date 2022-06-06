270f61e7c67f2943194fbb818c861cfb52b58aea
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-07 00:05:05 +0700

    create common page controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home', [
+            'path' => '/',
+            'title' => '',
+        ]);
+    }
+
+    public function project()
+    {
+        return view('project', [
+            'path' => '/project',
+            'title' => 'Portofolio -',
+        ]);
+    }
+
+    public function about()
+    {
+        return view('about', [
+            'path' => '/about',
+            'title' => 'Tentang -',
+        ]);
+    }
+}
