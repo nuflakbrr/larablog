972657a7aaead859e74e704d6c3c1739a35e749c
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-07 00:06:15 +0700

    implement eager loading and create relationship between table post and model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,9 +10,15 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+    protected $with = ['category'];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
