cb0edffe6986c4623e66a9085cf64ee851bac7bf
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-07 13:05:12 +0700

    set seeder test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,6 +34,76 @@ class DatabaseSeeder extends Seeder
             'published_at' => '2017-01-01 00:00:00',
         ]);
 
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Ini adalah excerpt Judul Kedua',
+            'body' => 'Ini adalah body Judul Kedua',
+            'published_at' => '2017-01-02 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Ini adalah excerpt Judul Ketiga',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-03 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Ini adalah excerpt Judul Keempat',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-04 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Kelima',
+            'slug' => 'judul-kelima',
+            'excerpt' => 'Ini adalah excerpt Judul Kelima',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-05 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Keenam',
+            'slug' => 'judul-keenam',
+            'excerpt' => 'Ini adalah excerpt Judul Keenam',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-06 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Ketujuh',
+            'slug' => 'judul-ketujuh',
+            'excerpt' => 'Ini adalah excerpt Judul Ketujuh',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-07 00:00:00',
+        ]);
+
+        \App\Models\Post::create([
+            'category_id' => 1,
+            'user_id' => 1,
+            'title' => 'Judul Kedelapan',
+            'slug' => 'judul-kedelapan',
+            'excerpt' => 'Ini adalah excerpt Judul Kedelapan',
+            'body' => 'Ini body Judul Kedua',
+            'published_at' => '2017-01-08 00:00:00',
+        ]);
+
         \App\Models\Category::create([
             'name' => 'Kategori Pertama',
             'slug' => 'kategori-pertama'
