c81443d6cc1654c286ce6a3ec0c79e64b9df35d8
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-07 21:09:58 +0700

    my-posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.admin')
+
+@section('container')
+    <div class="h-full ml-14 mt-14 mb-10 md:ml-64">
+        <div class="container px-5 py-3">
+            <div>
+                <h1 class="font-sans font-bold text-3xl text-white">Postingan Saya</h1>
+                <div class="border-b-2 border-b-gray-500 my-3" />
+            </div>
+
+            {{-- Table --}}
+            <div class="mt-4 mx-4">
+                <div class="w-full overflow-hidden rounded-lg shadow-xs">
+                    <div class="w-full overflow-x-auto">
+                        <table class="w-full">
+                            <thead>
+                                <tr
+                                    class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                                    <th class="px-4 py-3">#</th>
+                                    <th class="px-4 py-3">Judul</th>
+                                    <th class="px-4 py-3">Kategori</th>
+                                    <th class="px-4 py-3">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                                @foreach ($posts as $post)
+                                    <tr
+                                        class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                                        <td class="px-4 py-3 text-sm">{{ $loop->iteration }}</td>
+                                        {{-- <td class="px-4 py-3">
+                                            <div class="flex items-center text-sm">
+                                                <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
+                                                    <img class="object-cover w-full h-full rounded-full"
+                                                        src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+                                                        alt="" loading="lazy" />
+                                                    <div class="absolute inset-0 rounded-full shadow-inner"
+                                                        aria-hidden="true">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <p class="font-semibold">Hans Burger</p>
+                                                    <p class="text-xs text-gray-600 dark:text-gray-400">10x Developer</p>
+                                                </div>
+                                            </div>
+                                        </td> --}}
+                                        <td class="px-4 py-3 text-sm">{{ $post->title }}</td>
+                                        <td class="px-4 py-3 text-xs">
+                                            <span
+                                                class="flex xl:inline bg-blue-500 px-2 py-0.5 rounded-xl text-white font-bold whitespace-no-wrap">
+                                                {{ $post->category->name }}
+                                            </span>
+                                        </td>
+                                        <td class="flex px-4 py-3 text-sm">
+                                            <a href="/admin/my-posts/{{ $post->id }}">
+                                                <span
+                                                    class="flex bg-blue-500 hover:bg-blue-700 w-full text-white font-bold py-0.5 px-2 rounded-xl whitespace-no-wrap">
+                                                    <svg role="img" xmlns="http://www.w3.org/2000/svg" width="14"
+                                                        height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-eye">
+                                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                        <circle cx="12" cy="12" r="3"></circle>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                            <a href="#" class="ml-2">
+                                                <span
+                                                    class="flex bg-yellow-500 hover:bg-yellow-700 w-full text-white font-bold py-0.5 px-2 rounded-xl whitespace-no-wrap">
+                                                    <svg role="img" xmlns="http://www.w3.org/2000/svg" width="14"
+                                                        height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-edit">
+                                                        <path
+                                                            d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                                                        </path>
+                                                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
+                                                        </path>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                            <a href="#" class="ml-2">
+                                                <span
+                                                    class="flex bg-red-500 hover:bg-red-700 w-full text-white font-bold py-0.5 px-2 rounded-xl whitespace-no-wrap">
+                                                    <svg role="img" xmlns="http://www.w3.org/2000/svg" width="14"
+                                                        height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="feather feather-trash-2">
+                                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                                        <path
+                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                        </path>
+                                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div
+                        class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
+                        <span class="flex items-center col-span-3"> Showing 21-30 of 100 </span>
+                        <span class="col-span-2"></span>
+                        <!-- Pagination -->
+                        <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+                            <nav aria-label="Table navigation">
+                                <ul class="inline-flex items-center">
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                                            aria-label="Previous">
+                                            <svg aria-hidden="true" class="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                                    clip-rule="evenodd" fill-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">1</button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">2</button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 text-white dark:text-gray-800 transition-colors duration-150 bg-blue-600 dark:bg-gray-100 border border-r-0 border-blue-600 dark:border-gray-100 rounded-md focus:outline-none focus:shadow-outline-purple">3</button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">4</button>
+                                    </li>
+                                    <li>
+                                        <span class="px-3 py-1">...</span>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">8</button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple">9</button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
+                                            aria-label="Next">
+                                            <svg class="w-4 h-4 fill-current" aria-hidden="true" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                    clip-rule="evenodd" fill-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {{-- Table --}}
+        </div>
+    </div>
+@endsection
